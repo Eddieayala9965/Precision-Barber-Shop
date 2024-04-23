4a9940b169298acb1ef026ee11f635675e6607d3
Eddie Ayala <heriberto.ayala1981@gmail.com>
2024-04-23 19:25:57 -0400

    set up fastapi, time to take it easy for today

--- Backend/main.py
+++ Backend/main.py
@@ -1,1 +1,37 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
+from typing import List
+from datetime import datetime
+
+
+
+app = FastAPI()
+
+# CORS
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:8080",
+    "http://localhost:3000",
+    "http://localhost:5000",
+    "http://localhost:5500",
+    "http://localhost:8081",
+    "http://localhost:8082",
+    "http://localhost:8083",
+    "http://localhost:8084",
+    "http://localhost:8085",
+    ]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+)
+
+
