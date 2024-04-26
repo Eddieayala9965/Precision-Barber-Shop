a0183dc685193272737e3ec5848cbb6d38357039
Eddie Ayala <heriberto.ayala1981@gmail.com>
2024-04-26 02:01:38 -0400

    added the CheckoutForm.jsx, just need to implement it in my StripTest.jsx

---
+++ Frontend/src/components/CheckoutForm.jsx
@@ -0,0 +1,81 @@
+import { useState, useEffect } from "react";
+import {
+  CardElement,
+  PaymentElement,
+  useStripe,
+  useElements,
+  Elements,
+} from "@stripe/react-stripe-js";
+
+const CheckoutForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const [message, setMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (!stripe) {
+      return;
+    }
+    const clientSecret = new URLSearchParams(window.location.search).get(
+      "payment_intent_client_secret"
+    );
+    if (!clientSecret) {
+      return;
+    }
+    stripe.retrievePaymentIntent(clientSecret).then(({ paymentIntent }) => {
+      switch (paymentIntent.status) {
+        case "succeeded":
+          setMessage("Payment succeeded!");
+          break;
+        case "processing":
+          setMessage("Payment processing...");
+          break;
+        case "requires_payment_method":
+          setMessage("Payment failed: Please Try again.");
+          break;
+        default:
+          setMessage("Payment failed: Please Try again.");
+          break;
+      }
+    });
+  }, [stripe]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!stripe || !elements) {
+      return;
+    }
+    setIsLoading(true);
+
+    // make sure to change this to redirect to home page and add a success message ask chat for help
+    const { error } = await stripe.confirmPayment({
+      elements,
+      confirmParams: {
+        return_url: "http://localhost:3000",
+      },
+    });
+    if (error.type === "card_error" || error.type === "validation_error") {
+      setMessage(error.message);
+    } else {
+      setMessage("An unexpected error occured. Please try again.");
+    }
+    setIsLoading(false);
+  };
+  const paymentElementOptions = {
+    layout: "taabs",
+  };
+  return (
+    <form id="payment-form" onSubmit={handleSubmit}>
+      <PaymentElement id="payment-element" options={paymentElementOptions} />
+      <button disabled={isLoading || !stripe || !elements} id="submit">
+        <span id="button-text">
+          {isLoading ? <div className="spinner" id="spinner"></div> : "Pay now"}
+        </span>
+      </button>
+      {message && <div id="payment-message">{message}</div>}
+    </form>
+  );
+};
+export default CheckoutForm;
--- Frontend/src/routes/StripeTest.jsx
+++ Frontend/src/routes/StripeTest.jsx
@@ -1,8 +1,25 @@
+import { loadStripe } from "@stripe/stripe-js";
+import { useEffect, useState } from "react";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "../components/CheckoutForm";
+
 const StripeTest = () => {
+  useEffect(() => {
+    const initStripe = async () => {
+      const stripe = await loadStripe(
+        import.meta.env.VITE_STRIPE_APIKEY_FRONTEND
+      );
+      console.log(stripe);
+    };
+    initStripe();
+  }, []);
   return (
     <div>
       <h1>Stripe Test</h1>
+      <Elements stripe={stripe}>
+        <CheckoutForm />
     </div>
   );
 };
 export default StripeTest;
+//
