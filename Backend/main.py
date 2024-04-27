b4a2e82e3c112bb7a7ce228ebcd9c555b8e30c84
Eddie Ayala <heriberto.ayala1981@gmail.com>
2024-04-27 19:06:06 -0400

    added new things

--- Backend/main.py
+++ Backend/main.py
@@ -3,54 +3,64 @@ from fastapi.templating import Jinja2Templates
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from typing import List
 import stripe
-from stripe_test import stripe_key
+
+
+import os
+
+
+
+stripe.api_key = os.getenv("STRIPE_APIKEY_BACKEND")
 
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
-stripe.api_key = stripe_key
 
+
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="*", # origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 origins = [
     "http://localhost",
     "http://localhost:5137",
     ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["GET", "POST"],
-    allow_headers=["*"],
-)
-async def calculate_order_amount(items):
-    return sum(item["amount"] * item["quantity"] for item in items)
+class Item(BaseModel):
+    amount: int
+    quantity: int
+    price: int
+    
+def calculate_order_amount(items: List[Item]):
+    return sum(item.price * item.quantity for item in items)
 
 @app.get("/")
 def hello_world():
     return {"message": "hello world"}
 
-@app.post("/create-checkout-session")
-async def create_payment(request: Request):
-    try: 
-        data = await request.json()
-        amount = await calculate_order_amount(data['items'])
-        intent = stripe.PaymentIntent.create(
-            amount=amount, 
-            currency='usd',
-            automatic_payment_methods={'enabled': True},
+@app.post("/create-payment-intent")
+async def create_payment_intent(items: List[Item]):
+    try:
+        total_amount = calculate_order_amount(items)
+        payment_intent = stripe.PaymentIntent.create(
+            amount=int(total_amount * 100 ),
+            currency="usd",
         )
-        return JSONResponse({
-            'clientSecret': intent['client_secret']
-        })
-    except stripe.error.StripeError as e:
-        raise HTTPException(status_code=400, detail=str(e))
+        return payment_intent
     except Exception as e:
-        raise HTTPException(status_code=500, detail="An error occurred processing your payment.")
+        raise HTTPException(status_code=400, detail=str(e))
 
-if __name__ == '__main__':
-    app.run(port=4242)
+
+
+
+
     
 
 
--- Backend/stripe_test.py
+++
@@ -1,6 +0,0 @@
-import os
-import stripe
-
-
-stripe.api_key = os.getenv("STRIPE_APIKEY_BACKEND")
-stripe_key = stripe.api_key
