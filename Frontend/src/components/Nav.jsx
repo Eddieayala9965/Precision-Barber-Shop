9812d9c7755422a36b913946d8c5ce288b242fcb
Eddie Ayala <heriberto.ayala1981@gmail.com>
2024-06-19 20:36:03 -0400

    c

--- Frontend/src/components/Nav.jsx
+++ Frontend/src/components/Nav.jsx
@@ -102,7 +102,7 @@ const Nav = ({ navItems }) => {
                   component={Link}
                   to={link.path}
                 >
-                  <Typography textAlign="center">{link.title}</Typograph>
+                  <Typography textAlign="center">{link.title}</Typography>
                 </MenuItem>
               ))}
             </Menu>
