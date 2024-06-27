238594df231d8d137f3c044c9fbd7f7a7f1f43c9
Eddie Ayala <heriberto.ayala1981@gmail.com>
2024-06-27 02:03:03 -0400

    d

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -33,7 +33,7 @@ const router = createBrowserRouter([
         element: <StripeTest />,
       },
       {
-        path: "/contact",
+        path: "/contac",
         element: <Contact />,
       },
     ],
